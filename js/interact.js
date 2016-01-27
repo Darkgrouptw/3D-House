a6a83b3cee9dacf88cd514f29b916474216fdca7
Chia-Yu <kchangkoko@gmail.com>
2016-01-27 19:44:07 +0800

    新增Subtoolbar 動態content

--- js/interact.js
+++ js/interact.js
@@ -1,3 +1,11 @@
+function ExportClick()
+{
+    GetStyle('ExportContent').display = "block";
+}
+function CloseExport()
+{
+    GetStyle('ExportContent').display = "none";
+}
 var openflag = true;
 
 function MainbtnClick() 
@@ -10,9 +18,8 @@ function MainbtnClick()
 
 function BtnAnimation(open)
 {
-    var mainsize = Math.round(GetElement('Main').clientHeight);
-    var bottomsize = mainsize/6;
-    var btnsize = bottomsize/2;
+    var bottomsize = Math.round(window.innerHeight/6);
+    var btnsize = Math.round(bottomsize/2);
     var img = GetElement('mainbtnimg');
     if (open) 
     {
@@ -28,7 +35,7 @@ function BtnAnimation(open)
     }
     
 }
-
+//Toolbar
 function SubdecoratebtnClick()
 {
     GetStyle('Toolbar').display = "block";
@@ -41,4 +48,36 @@ function CloseToolbarClick()
     GetStyle('Subaddbtn').bottom = GetStyle('Mainbtn').bottom;
     GetStyle('Subdecoratebtn').right = GetStyle('Mainbtn').right;
     GetElement('mainbtnimg').src = "./images/add.png";
+}
+//SubToolbar
+function CloseSubToolbarClick()
+{
+    GetStyle('SubToolbar').display = "none";
+}
+function SubToolbarClick(type, index){
+    console.log(index);
+}
+function RoofClick(){
+   
+    //new element to SubToolbar dynamically
+    var AddElem = function(index, MajorElem , BeforeElem)
+    {
+        var E = document.createElement("li");
+        var context = document.createTextNode("Roof" + index.toString());
+        E.id = "Roof" + index.toString();
+        E.onclick = function() {SubToolbarClick(2,index)};
+        E.appendChild(context);
+        
+        MajorElem.insertBefore(E, BeforeElem);
+    }
+    var totalroof = 3;
+    var content = GetElement('SubToolbarContent');
+    var insertElement = GetElement('SubToolbarClose');
+    
+    for (var i = 1; i <= 3; i++)
+    {
+        AddElem(i,content,insertElement);
+    }
+        GetStyle('SubToolbar').display = "block";
+    
 }
