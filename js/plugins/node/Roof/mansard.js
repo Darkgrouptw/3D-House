908977ad33b547019c12a06f4014958c3b844554
Dark <darkgrouptw@gmail.com>
2016-02-01 14:01:48 +0800

    矩陣運算完成

---
+++ js/plugins/node/Roof/mansard.js
@@ -0,0 +1,199 @@
+(function () {
+
+    SceneJS.Types.addType("roof/mansard", 
+	{ construct:function (params) { this.addNode(build.call(this, params)); }});
+
+    function build(params) 
+	{
+        var size = {x: 0, y: 0, z: 0}, ratio = { a: 0, b: 0 }, center, thick;
+        if(params.size && params.ratio) 
+		{ 
+			size.x = params.size.x / 2;
+			size.y = params.size.z / 2;
+			size.z = params.size.y / 2;
+			ratio = params.ratio; 
+		} 
+
+		if(params.thick) { thick = params.thick } else { thick = 1; }
+		
+		if(params.center) { center = params.center; }
+		else { center = { x: 0, y: 0, z: 0}; }
+		
+        var coreId = "roof/mansard" + "_" + SceneJS_add_randomString(20) + "_" + (params.wire ? "wire" : "_solid");
+
+        // If a node core already exists for a prim with the given properties,
+        // then for efficiency we'll share that core rather than create another geometry
+        if (this.getScene().hasCore("geometry", coreId)) { return { type:"geometry", coreId:coreId }; }
+		
+		
+		var sZ = size.z, sX = size.x, sY = size.y;
+		
+		// Abbreviate to easy use. dnxpr is mean the "'d'ouble 'n'egative 'x' and multply 'r'atio.x"
+		// The same, p is mean the positive
+		var dNxr = 2 * -sX * ratio.a, dPxr = 2 * sX * ratio.a, 
+			dNzr = 2 * -sZ * ratio.b, dPzr = 2 * sZ * ratio.b;
+		
+		// It is mean the " 's'ize.'z' 'm'inus 't'hick "
+		var sYmt = sY - thick;
+		
+		var positionSet = new Float32Array(
+		[
+			// Front  West
+			-sX - dNxr, sY, sZ - dPzr, 
+			-sX, -sY, sZ, 										// A
+			-sX, -sY, -sZ, 										// C
+			-sX - dNxr, sY, -sZ - dNzr,
+			
+			// Front Sourth
+			sX - dPxr, sY, -sZ - dNzr, 		
+			-sX - dNxr, sY, -sZ - dNzr, 
+			-sX, -sY, -sZ, 										// C
+			sX, -sY, -sZ, 										// D
+			
+			// Front East
+			sX - dPxr, sY, sZ - dPzr, 
+			sX - dPxr, sY, -sZ - dNzr, 
+			sX, -sY, -sZ, 										// D
+			sX, -sY, sZ, 										// B
+			
+			// Front North
+			sX - dPxr, sY, sZ - dPzr, 
+			sX, -sY, sZ, 										// B
+			-sX, -sY, sZ, 										// A
+			-sX - dNxr, sY, sZ - dPzr, 
+			
+			// Front Center
+			-sX - dNxr, sY, -sZ - dNzr, 
+			sX - dPxr, sY, -sZ - dNzr, 
+			sX - dPxr, sY, sZ - dPzr, 
+			-sX - dNxr, sY, sZ - dPzr, 
+			
+			// Back West
+			-sX - dNxr + thick, sYmt, sZ - dPzr - thick, 
+			-sX - dNxr + thick, sYmt, -sZ - dNzr + thick, 
+			-sX + thick, -sY, -sZ + thick, 					// C'
+			-sX + thick, -sY, sZ - thick, 					// A'
+			
+			// Back Sourth
+			sX - dPxr - thick, sYmt, -sZ - dNzr + thick, 
+			sX- thick, -sY, -sZ + thick, 					// D'
+			-sX + thick, -sY, -sZ + thick, 					// A'
+			-sX - dNxr + thick, sYmt, -sZ - dNzr + thick, 
+			
+			// Back East
+			sX - dPxr - thick, sYmt, sZ - dPzr - thick, 
+			sX - thick, -sY, sZ - thick, 						// B'
+			sX - thick, -sY, -sZ + thick, 						// D'
+			sX - dPxr - thick, sYmt, -sZ - dNzr + thick, 
+			
+			// Back North
+			sX - dPxr - thick, sYmt, sZ - dPzr - thick, 
+			-sX - dNxr + thick, sYmt, sZ - dPzr - thick, 
+			-sX + thick, -sY, sZ - thick, 						// A'
+			sX - thick, -sY, sZ - thick, 						// B'
+			
+			// Back Center
+			sX - dPxr - thick, sYmt, sZ - dPzr - thick, 
+			sX - dPxr - thick, sYmt, -sZ - dNzr + thick, 
+			-sX - dNxr + thick, sYmt, -sZ - dNzr + thick, 
+			-sX - dNxr + thick,  sYmt, sZ - dPzr - thick,
+			
+			// Side West
+			-sX + thick, -sY, sZ - thick, 
+			-sX + thick, -sY, -sZ + thick, 
+			-sX, -sY, -sZ, 	
+			-sX, -sY, sZ,
+			
+			// Side Sourth
+			sX - thick, -sY, -sZ + thick, 	
+			sX, -sY, -sZ, 	
+			-sX, -sY, -sZ, 
+			-sX + thick, -sY, -sZ + thick,
+			
+			// Side East
+			sX - thick, -sY, sZ - thick, 
+			sX, -sY, sZ,
+			sX, -sY, -sZ, 
+			sX - thick, -sY, -sZ + thick, 	
+			
+			// Side North
+			sX - thick, -sY, sZ- thick, 
+			-sX + thick, -sY, sZ - thick, 			
+			-sX, -sY, sZ, 
+			sX, -sY, sZ, 
+		]);
+		
+		for(var pidx = 0; pidx < positionSet.length; pidx = pidx + 3)
+		{
+			positionSet[pidx] = positionSet[pidx] + center.x;
+			positionSet[pidx + 1] = positionSet[pidx + 1] + center.y;
+			positionSet[pidx + 2] = positionSet[pidx + 2] + center.z;
+		}
+		
+        // Otherwise, create a new geometry
+        var newone = 
+		{
+            type: "geometry",
+            primitive: params.wire ? "lines" : "triangles",
+            coreId: coreId,
+            positions: positionSet,
+			normals: new Float32Array([
+				0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,
+                0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
+                -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0,
+                0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0,
+				
+				0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,
+                0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
+                -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0,
+                0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0,
+				
+				0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,
+                0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
+                -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0,
+			]),
+			uv: new Float32Array([
+				/*0, 1, 0, 0, 1, 0, 1, 1,			// Front West
+				1, 1, 1, 0, 0, 0, 0, 1,			// Front Sourth
+				0, 1, 1, 1, 1, 0, 0, 0,			// Front East
+				0, 1, 0, 0, 1, 0, 1, 1,			// Front North
+				1, 1, 1, 0, 0, 0, 0, 1,			// Front Center
+				
+				0, 1, 0, 0, 1, 0, 1, 1,			// Back West
+				1, 1, 0, 1, 0, 0, 1, 1,			// Back Sourth
+				1, 0, 0, 0, 0, 1, 1, 1,			// Back East
+				1, 1, 0, 1, 0, 0, 1, 0,			// Back North
+				0, 1, 1, 1, 1, 0, 0, 0			// Back Center
+				
+				0, 1, 0, 0, 1, 0, 1, 1,			// Side West
+				1, 1, 0, 1, 0, 0, 1, 1,			// Side Sourth
+				1, 0, 0, 0, 0, 1, 1, 1,			// Side East
+				1, 1, 0, 1, 0, 0, 1, 0,			// Side North*/
+			]),
+            indices: 
+			[
+				0, 1, 2, 0, 2, 3,				// Front West
+				4, 5, 6, 4, 6, 7,				// Front Sourth
+				8, 9, 10, 8, 10, 11,			// Front East
+				12, 13, 14, 12, 14, 15,			// Front North
+				16, 17, 18, 16, 18, 19,			// Front Center
+				
+				20, 21, 22, 20, 22, 23,			// Back West
+				24, 25, 26, 24, 26, 27,			// Back Sourth
+				28, 29, 30, 28, 30, 31,			// Back East
+				32, 33, 34, 32, 34, 35,			// Back North
+				36, 37, 38, 36, 38, 39,			// Back Center
+				
+				40, 41, 42, 40, 42, 43,			// Side West
+				44, 45, 46, 44, 46, 47,			// Side Sourth
+				48, 49, 50, 48, 50, 51,			// Side East
+				52, 53, 54, 52, 54, 55			// Side North
+            ]
+        };
+		
+		return newone;
+    }
+})();
--- js/plugins/node/Wall/Triangle.js
+++ js/plugins/node/Wall/Triangle.js
@@ -1,157 +1,56 @@
-var triangle_w,triangle_h,triangle_rx,triangle_ry,triangle_th;
-    SceneJS.Types.addType("Wall/Triangle", 
-	{
-        construct:function (params) { this.addNode(build.call(this, params)); },
-        setCenter:function(point){
-        	var matrix= this.parent;
-            matrix=matrix.parent;
-            var mat=matrix.getWorldMatrix();
-            mat[12]=point.x;
-            mat[13]=point.y;
-            mat[14]=point.z;
-            matrix.setMatrix(mat);
-        },
-        getCenter:function(){
-        	var matrix=this.parent;
-        	matrix=matrix.parent;
-        	var mat=matrix.getWorldMatrix();
+(function () {
 
-        	var cx=mat[12],cy=mat[13],cz=mat[14];
-            return {x: cx, y: cy, z: cz };
-        },
-        setCenterX:function(x){
-            var matrix= this.parent;
-            matrix=matrix.parent;
-            var mat=matrix.getWorldMatrix();
-            mat[12]=x;
-            matrix.setMatrix(mat);
-        },
-        setCenterY:function(y){
-            var matrix= this.parent;
-            matrix=matrix.parent;
-            var mat=matrix.getWorldMatrix();
-            mat[13]=y;
-            matrix.setMatrix(mat);
-        },
-        setCenterZ:function(z){
-            var matrix= this.parent;
-            matrix=matrix.parent;
-            var mat=matrix.getWorldMatrix();
-            mat[14]=z;
-            matrix.setMatrix(mat);
-        },
-        setWidth:function(wide){
-            var geometry= this.findNodesByType("geometry")[0];
-            triangle_w=wide*1;
-            positionSet=generaterPositionSetForTriangle();
-            geometry.setPositions({positions: positionSet});
-        },
-        getWidth:function(){
-            return triangle_w*1;
-        },
-        setHeight:function(high){
-            var geometry= this.findNodesByType("geometry")[0];
-            triangle_h=high*1;
-            positionSet=generaterPositionSetForTriangle();
-            geometry.setPositions({positions: positionSet});
-        },
-        getHeight:function(){
-            return triangle_h*1;
-        },
-        setThickness:function(thic){
-            var geometry= this.findNodesByType("geometry")[0];
-            triangle_th=thic*1;
-            positionSet=generaterPositionSetForTriangle();
-            geometry.setPositions({positions: positionSet});
-        },
-        getThickness:function(){
-            return triangle_th*1;
-        },
-        callBaseCalibration:function(){
-
-        	return;
-            var leftTriangle=-1;
-            var rightTriangle=-1;
-            var roof=-1;
-            var nodes=scene.findNodes();
-            for(var i=0;i<nodes.length;i++){
-                var n = nodes[i];
-                if(n.getType()=="name"){
-                    if(n.getName()=="roof")roof=n.nodes[0].nodes[0].nodes[0].nodes[0].nodes[0];
-                    else if(n.getName()=="leftTriangle")leftTriangle=n.nodes[0].nodes[0].nodes[0].nodes[0].nodes[0];
-                    else if(n.getName()=="rightTriangle")rightTriangle=n.nodes[0].nodes[0].nodes[0].nodes[0].nodes[0];
-                }
-            }
-            if(roof == -1){
-                console.log("ERROR");
-                return;
-            }
-            if(leftTriangle.getID() == this.getID()){
-            	roof.setWidth(this.getWidth());
-            	roof.setHeight(this.getHeight());
-            	roof.adjustChildren();
-            }else if(rightTriangle.getID() == this.getID()){
-            	roof.setWidth(this.getWidth());
-            	roof.setHeight(this.getHeight());
-            	roof.adjustChildren();
-            }
-        }
-    });
+    SceneJS.Types.addType("wall/triangle", { construct:function (params) { this.addNode(build.call(this, params)); } });
 
     function build(params) 
 	{
-        var width, height, thickness, center, ratio;
-        if(params.width && params.height && params.thickness) 
+        var width, height, thick, center, ratio;
+        if(params.size && params.thick) 
 		{
-            width = params.width;
-            height = params.height;
-            thickness = params.thickness;
+            width = (params.size.a || params.size.y) / 2;
+            height = (params.size.b || params.size.z) / 2;
+            thick = params.thick / 2;
         } 
-		else { width = 0; height = 0; thickness = 0; }
+		else { width = 0; height = 0; thick = 0; }
 		
 		if(params.center) { center = params.center; }
 		else { center = { x: 0, y: 0, z: 0}; }
 		
 		if(params.ratio) { ratio = params.ratio; }
-		else { ratio = { x: 0.5, y: 0.5, z: 1.0 }; }
-        var coreId = "Wall/Triangle" + width + "_" + height + "_" + thickness + (params.wire ? "wire" : "_solid");
+		else { ratio = { a: 0, b: 0}; }
+
+        var coreId = "wall/triangle" + "_" + SceneJS_add_randomString(20) + "_" + (params.wire ? "wire" : "solid");
 
         // If a node core already exists for a prim with the given properties,
         // then for efficiency we'll share that core rather than create another geometry
         if (this.getScene().hasCore("geometry", coreId)) { return { type:"geometry", coreId:coreId }; }
 
-        //kasim
-        triangle_w=width;triangle_h=height;triangle_rx=ratio.x;triangle_ry=ratio.y;triangle_th=thickness;
-
-		positionSet = new Float32Array(
+		var positionSet = new Float32Array(
 		[
-			// Left Triangle
-			-width, -height, -thickness,
-			width, -height, -thickness,
-			(width * ratio.x + -width * ratio.y) / 2, height, -thickness,
+			-width, -height, -thick,									// A
+			width, -height, -thick,										// B
+			(width * ratio.a + -width * ratio.b) / 2, height, -thick,	// C
 			
-			// Back Face
-			width, -height, -thickness,
-			(width * ratio.x + -width * ratio.y) / 2, height, -thickness,
-			(width * ratio.x + -width * ratio.y) / 2, height, thickness,
-			width, -height, thickness,
+			-width, -height, thick,										// A'
+			width, -height, thick,										// B'
+			(width * ratio.a + -width * ratio.b) / 2, height, thick		// C'
 			
-			// Front Face
-			-width, -height, -thickness,
-			(width * ratio.x + -width * ratio.y) / 2, height, -thickness,
-			(width * ratio.x + -width * ratio.y) / 2, height, thickness,
-			-width, -height, thickness,
+			/*
+			-width, -height, thick,
+			-width, -height, -thick,
+			width, -height, -thick,
+			width, -height, thick,	
 			
-			// Buttom Face
-			-width, -height, -thickness,
-			width, -height, -thickness,
-			width, -height, thickness,
-			-width, -height, thickness,
-		
-			// Right Triangle
-			-width, -height, thickness,
-			width, -height, thickness,
-			(width * ratio.x + -width * ratio.y) / 2, height, thickness
+			-width, -height, thick,
+			-width, -height, -thick,
+			(width * ratio.a + -width * ratio.b) / 2, height, -thick,
+			(width * ratio.a + -width * ratio.b) / 2, height, thick,
+			
+			(width * ratio.a + -width * ratio.b) / 2, height, thick,
+			(width * ratio.a + -width * ratio.b) / 2, height, -thick,
+			width, -height, -thick,
+			width, -height, thick,
+			*/
 		]);
 		
 		for(var pidx = 0; pidx < positionSet.length; pidx = pidx + 3)
@@ -162,72 +61,23 @@ var triangle_w,triangle_h,triangle_rx,triangle_ry,triangle_th;
 		}
 		
         // Otherwise, create a new geometry
-        newone = 
+        var newone = 
 		{
             type: "geometry",
             primitive: params.wire ? "lines" : "triangles",
             coreId: coreId,
             positions: positionSet,
-            normals: new Float32Array([
-				0,0,-1,0,0,-1,0,0,-1,
-				0,0,0,0,0,0,0,0,0,0,0,0,
-				0,0,0,0,0,0,0,0,0,0,0,0,
-				0,0,0,0,0,0,0,0,0,0,0,0,
-				0,0,1,0,0,1,0,0,1
-			]),
-			uv: new Float32Array([
-				0,0,1,0,ratio.x,1,
-				1,0,1,1,0,1,0,0,
-				1,1,0,1,0,0,1,0,
-				0,1,0,0,1,0,1,1,
-				0,0,1,0,ratio.y,1
-			]),
+            normals: "auto",
             indices: 
 			[
-				0, 1, 2,
-				3, 4, 5, 3, 5, 6,
-				7, 8, 9, 7, 9, 10,
-				11, 12, 13, 11, 13, 14,
-				15, 16, 17,
+				2, 1, 0, 			// Front
+				3, 4, 5,			// Back
+				3, 5, 2, 3, 0, 2,	// Side A
+				3, 0, 1, 3, 4, 1,	// Side B
+				2, 5, 4, 2, 1, 4	// Side C
             ]
         };
 		
 		return newone;
     }
-
-function generaterPositionSetForTriangle(){
-	var width,height,ratioX,ratioY,thickness;
-	width=triangle_w;height=triangle_h;ratioX=triangle_rx;ratioY=triangle_ry;thickness=triangle_th;
-
-	positionSet = new Float32Array(
-		[
-			// Left Triangle
-			-width, -height, -thickness,
-			width, -height, -thickness,
-			(width * ratioX + -width * ratioY) / 2, height, -thickness,
-			
-			// Back Face
-			width, -height, -thickness,
-			(width * ratioX + -width * ratioY) / 2, height, -thickness,
-			(width * ratioX + -width * ratioY) / 2, height, thickness,
-			width, -height, thickness,
-			
-			// Front Face
-			-width, -height, -thickness,
-			(width * ratioX + -width * ratioY) / 2, height, -thickness,
-			(width * ratioX + -width * ratioY) / 2, height, thickness,
-			-width, -height, thickness,
-			
-			// Buttom Face
-			-width, -height, -thickness,
-			width, -height, -thickness,
-			width, -height, thickness,
-			-width, -height, thickness,
-		
-			// Right Triangle
-			-width, -height, thickness,
-			width, -height, thickness,
-			(width * ratioX + -width * ratioY) / 2, height, thickness
-		]);
-	return positionSet;
-}
+})();
